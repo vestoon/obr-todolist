4f1f6ebac4330135d8578bf583b732e6dcd19285
bestoon <zzzz6747@naver.com>
2025-06-09 08:48:40 +0900

    Editor.css 구현

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Editor.css';
 
 export const Editor = () => {
 
     return (
-        <div>
-            <input></input>
+        <div className="Editor">
+            <input placeholder='새로운 Todo...'></input>
             <button>추가</button>
         </div>
     )
