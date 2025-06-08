beef6ddb893bccf23cb605579cf236b325d27d55
bestoon <zzzz6747@naver.com>
2025-06-09 08:29:44 +0900

    Header.css 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Header.css';
 
 export const Header = () => {
     const today = new Date();
 
     return (
-        <div>
-            오늘은🗓️
-            {today.toDateString()}
+        <div className="Header">
+            <h3>오늘은🗓️</h3>
+            <h1>{today.toDateString()}</h1>
         </div>
     )
 }
