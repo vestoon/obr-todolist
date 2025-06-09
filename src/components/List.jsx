ecd5960208c2d3c024a7931e0f3fc72209a00a8c
bestoon <zzzz6747@naver.com>
2025-06-09 09:02:26 +0900

    List.css 구현

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import {TodoList} from './TodoItem'
+import './List.css';
 
 export const List = () => {
 
     return (
-        <div>
-            Todo List🌱
-            <input></input>
+        <div className="List">
+            <h3>Todo List🌱</h3>
+            <input placeholder="검색어를 입력하세요"></input>
+            <TodoList></TodoList>
+            <TodoList></TodoList>
+            <TodoList></TodoList>
         </div>
     )
 }
